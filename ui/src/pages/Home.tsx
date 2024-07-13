41c2b434f86e5afb170e39a59fea1c237d54a3fa
panosdb <ppsimatikas@gmail.com>
2024-07-13 19:54:38 +0200

    Fix home

--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,6 +1,13 @@
 import {Stack, Text, Title} from "@mantine/core";
 import {HomeMap} from "../components/maps/home_map";
 import {LaunchDAppButton} from '../components/launchDappButton/launchDappButton';
+import {useEffect, useState} from "react";
+
+const messages = [
+    "Agriculture is a $4.6 trillion dollar industry",
+    "50% of Habitable land is used for Agriculture",
+    "309 million people are facing acute levels of food insecurity"
+]
 
 function Home() {
     const [messageIndex, setMessageIndex] = useState(0);
@@ -13,14 +20,14 @@ function Home() {
     }, []);
 
     return (
-      <Stack style={{ minHeight: '100vh' }} align="center" justify="flex-start">
-        <Stack align="center" style={{ marginTop: '10px' }}>
-          <Title c="white">Welcome to Demeter AI</Title>
-          <Text c="white" mt={10}>50% of Habitable land is used for Agriculture</Text>
+        <Stack style={{minHeight: '100vh'}} align="center" justify="flex-start">
+            <Stack align="center" style={{marginTop: '10px'}}>
+                <Title c="white">Welcome to Demeter AI</Title>
+                <Text c="white" mt={10}>{messages[messageIndex]}</Text>
+            </Stack>
+            <HomeMap index={messageIndex}/>
+            <LaunchDAppButton/>
         </Stack>
-        <HomeMap />
-        <LaunchDAppButton />
-      </Stack>
     );
 }
 
